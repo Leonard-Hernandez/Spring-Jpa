618baf78f5aeb62896cebe7594340edecfca8e52
Leonard <Leonardhernandez02@gmail.com>
2025-02-23 21:49:19 -0500

    creando un nuevo objeto Person en la db por  terminal

--- src/main/java/com/andres/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/andres/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,11 +1,13 @@
 package com.andres.curso.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.andres.curso.springboot.jpa.springboot_jpa.entities.Person;
 import com.andres.curso.springboot.jpa.springboot_jpa.repositories.PersonRepository;
@@ -23,14 +25,18 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		create();
+
 		findOne();
 
 	}
 
+	@Transactional(readOnly = true)
 	public void findOne(){
-		personRepository.findByNameContaining("ria").ifPresent(System.out::println);
+		personRepository.findByNameContaining("nard").ifPresent(System.out::println);
 	}
 
+	@Transactional(readOnly = true)
 	public void list(){
 		
 		List<Person> persons = (List<Person>) personRepository.buscarByProgrammingLanguage("Java");
@@ -43,6 +49,24 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		personData.stream().forEach(person -> {
 			System.out.println(person[0] + " es experto en " + person[1]);
 		});
+	}
+
+	@Transactional
+	public void create(){
+		
+		Person person = new Person();
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Ingrese el nombre de la persona: ");
+		person.setName(scanner.nextLine());
+		System.out.println("Ingrese el apellido de la persona: ");
+		person.setlastname(scanner.nextLine());
+		System.out.println("Ingrese el lenguaje de programacion de la persona: ");
+		person.setProgrammingLanguage(scanner.nextLine());
+
+		personRepository.save(person);
+
+		scanner.close();
 	}
 
 }
